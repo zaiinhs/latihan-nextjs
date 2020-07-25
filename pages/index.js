9654d7b5ccb32a760e539793c8f1b63ab98f542e
zainalabidinhs <zainalabidin.tongas@gmail.com>
2020-07-25 09:44:48 +0700

    perubahan 2

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,13 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../public/components/layout'
-import utilStyles from '../public/styles/utils.module.css'
+import Layout from '../public/components/layout'
 
 export default function Home() {
   return (
-    <Layout home>
+    <div>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Zainal</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[hello guys]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
+      <Layout />
+    </div>
   )
 }
--- public/components/layout.js
+++ public/components/layout.js
@@ -1,66 +1,20 @@
-import Head from 'next/head'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Zainal Abidin'
-export const siteTitle = 'Next.js Sample Website'
-
-export default function Layout({ children, home }) {
+export default function Layout() {
     return (
         <div className={styles.container}>
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-                <meta
-                    name="description"
-                    content="Learn how to build a personal website using Next.js"
-                />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.now.sh/${encodeURI(
-                        siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
-            </Head>
-            <header className={styles.header}>
-                {home ? (
-                    <>
-                        <img
-                            src="/images/profile.jpg"
-                            className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-                            alt={name}
-                        />
-                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
-                    </>
-                ) : (
-                        <>
-                            <Link href="/">
-                                <a>
-                                    <img
-                                        src="/images/profile.jpg"
-                                        className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                                        alt={name}
-                                    />
-                                </a>
-                            </Link>
-                            <h2 className={utilStyles.headingLg}>
-                                <Link href="/">
-                                    <a className={utilStyles.colorInherit}>{name}</a>
-                                </Link>
-                            </h2>
-                        </>
-                    )}
-            </header>
-            <main>{children}</main>
-            {!home && (
-                <div className={styles.backToHome}>
-                    <Link href="/">
-                        <a>← Back to home</a>
-                    </Link>
-                </div>
-            )}
+            <img
+                src="/images/profile.jpg"
+                className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+            />
+            <h1 className={utilStyles.namamu}>Zainal Abidin</h1>
+            <div className={utilStyles.modeldeskripsi}>
+                <p>Hello guys, my name is Zainal Abidin</p>
+                <p>My current activity is studying at a university in Probolinggo-JawaTimur</p>
+                <p className={styles.paragraflink}>(please see the results of the project that I'm working on by visiting <a href="https://github.com/zainalabidinhs">my github profile account</a>.)</p>
+            </div>
         </div>
     )
 }
